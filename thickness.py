30c4744e55e6ff0e08075c356d8c0979ac5b4cf6
ghantesh <maan.anurag@gmail.com>
2019-07-01 19:17:56 -0400

    cleaned up thickness.py by removing vestiges of serial execution and plotting tools

--- thickness.py
+++ thickness.py
@@ -15,75 +15,6 @@ from functools import partial
 import csv
 import sys
 
-def calc_OBS(shells,rukawat,src,elem,elem_r,elem_noruk,elem_noruk_t,thetas,i):
-    #for ii in range(i*n,(i+1)*n):
-        # replace this with elem
-        # get an element p
-    p = get.p(shells.x[i,:],shells.y[i,:],shells.z[i,:])
-        
-        # get coordinates of center of the element
-    c_p = get.center(p['p1'],p['p2'],p['p3'])
-        
-        # Get Normal to element
-    N_p = get.normal(p['p1'],p['p3'],p['p2'])
-        
-        # get ray from source
-        
-        #ray = get.sourcevector(src['NO'],c_p)
-    ray = get.sourcevector(src['NO'],c_p)
-        
-        # get angle between ray and element
-    m = 0
-    n = 0
-    ray_angle = get.angle(N_p,ray['d'])
-        # ray angle less than 90 implies source faces the face
-    if ray_angle < 90:  
-            
-            # Check for obstruction with evaporator assembly and centerstack-stack
-            # loop through all elements in rukawat.
-    #        print('now checking against all rukawat', i)
-        for j in range(0,elem_r):
-                # get an element from rukawat
-            rwat = get.p(rukawat.x[j,:],rukawat.y[j,:],rukawat.z[j,:])
-                # get center of rukawat element
-            c_rwat = get.center(rwat['p1'],rwat['p2'],rwat['p3'])
-                # get normal to obstruction/rukawat
-            N_rwat = get.normal(rwat['p1'],rwat['p3'],rwat['p2'])
-                # Find Q, the point along ray that lies on the plane of rwat
-            Q = get.rwatQ(N_rwat,ray,c_rwat)
-                # Check if Q belongs to rwat and if yes is t_Q < t
-            OBS = get.obstruction(rwat,Q,ray['t'],N_rwat)          
-            if OBS == True:
-    #                print('Obstruction found in rukawat, break',i)
-                break
-            else:
-                m = m+1
-            
-                
-            # check for obstruction from shell elements
-        if OBS == False:
-    #            print('now checking against all shell elements', i)
-                
-            for k in [u for u in range(0,elem) if u!=i]:
-                rwat = get.p(shells.x[k,:],shells.y[k,:],shells.z[k,:])
-                c_rwat = get.center(rwat['p1'],rwat['p2'],rwat['p3'])
-                N_rwat = get.normal(rwat['p1'],rwat['p3'],rwat['p2'])
-                Q = get.rwatQ(N_rwat,ray,c_rwat)
-                OBS = get.obstruction(rwat,Q,ray['t'],N_rwat)            
-                if OBS == True:
-    #                    print('Obstruction found in shells, break',i)
-                    break
-                else:
-                    n = n+1
-                
-        if OBS == False:
-            #print "no obstruction found in element",i,m,n 
-            elem_noruk = np.append(elem_noruk,i)
-            thetas = np.append(thetas,ray_angle)
-            elem_noruk_t = np.append(elem_noruk_t,ray['t'])
-
-    return {'i':elem_noruk, 'thetas':thetas, 't':elem_noruk_t}        
-
 def calc_OBS_para(shells,rukawat,src,elem,elem_r,elem_noruk,elem_noruk_t,thetas,n,i):
     for ii in range(i*n,(i+1)*n):
                     # replace this with elem
@@ -177,25 +108,6 @@ elem_r = np.size(rukawat.areas)     # number of elements in rukawat
 
 
 start = time.time()
-"""
-# Parallel Execution pool
-
-num_cores = 8
-diagnostics.write("Number of cores %s\r\n" %num_cores)
-n = elem/num_cores
-iterable = list(range(num_cores))
-pool = mp.Pool(num_cores)
-OUTPUT = dict(i=np.array([]),thetas=np.array([]),t = np.array([]))
-#print("Number of elements", elem)
-#print("Number of Obstructions", elem_r)
-diagnostics.write("Number of elements -  %s\r\n" %elem)
-diagnostics.write("Number of Obstructions - %s\r\n" %elem_r)
-func = partial(calc_OBS_para,shells,rukawat,src,np.size(shells.areas),elem_r,OUTPUT['i'],OUTPUT['t'],OUTPUT['thetas'],n)
-OP = pool.map(func,iterable)
-pool.close()
-pool.join()
-# Stitch outputs together
-"""
 
 # Parallel Execution process
 
@@ -226,21 +138,7 @@ with open('OUTPUT.DAT','w+') as OP:
     op_writer = csv.writer(OP, delimiter='\t')
     for i in range(0,np.size(OUTPUT['i'])):
         op_writer.writerow([OUTPUT['i'][i],OUTPUT['thetas'][i],OUTPUT['t'][i]])
-        
 
-        
-"""
-# Series Execution
-elem_noruk = np.array([])
-elem_noruk_t = np.array([])
-thetas = np.array([])
-for i in range(0,elem):
-    OUTPUT = calc_OBS(shells,rukawat,src,elem,elem_r,elem_noruk,elem_noruk_t,thetas,i)
-    elem_noruk = OUTPUT['i']
-    thetas = OUTPUT['thetas']
-    elem_noruk_t = OUTPUT['t']
- 
-"""
 print("Execution time --- %s seconds ---\r\n" % (time.time() - start))         
 diagnostics.write("Execution time --- %s seconds ---\r\n" % (time.time() - start))
 diagnostics.write("Number of elements with no obstruction - %s \r\n" %np.size(OUTPUT['i']))
@@ -266,13 +164,8 @@ for e in OUTPUT['i']:
     i = i+1
 ax.scatter(src['NO'][0],src['NO'][1],src['NO'][2],s=50,c='r')
 get.view(ax,3)
-'''
-ax.set_zlim(-2,2)
-ax.set_ylim(-2,4)
-ax.set_xlim(0,10)
-plt.show()
 
-'''
+
 ax.set_zlim(-400,400)
 ax.set_ylim(-500,500)
 ax.set_xlim(-500,500)
